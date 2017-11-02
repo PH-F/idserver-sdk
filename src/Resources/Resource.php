b9a258c761223bddcbf7b44e696d69c5e5bd4d90
Junior Grossi <juniorgro@gmail.com>
2017-11-02 11:20:20 -0200

    Implement a generic call method returning an array

--- src/Resources/Resource.php
+++ src/Resources/Resource.php
@@ -18,4 +18,24 @@ abstract class Resource
     {
         $this->client = $client;
     }
+
+    /**
+     * @param string $method
+     * @param string $uri
+     * @param array $params
+     * @return array
+     */
+    protected function call(string $method, string $uri, array $params = []): array
+    {
+        $option = strtoupper($method) === 'GET' ?
+            'query' : 'form_params';
+
+        $response = $this->client->request($method, $uri, [
+            $option => $params,
+        ]);
+
+        return json_decode(
+            $response->getBody()->getContents(), true
+        );
+    }
 }
