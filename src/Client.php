4b6ab908dfe465494b05b2eddd1f7315e2174dd1
Junior Grossi <juniorgro@gmail.com>
2017-11-02 13:08:38 -0200

    Make Client class simple

--- src/Client.php
+++ src/Client.php
@@ -2,39 +2,10 @@
 
 namespace Xingo\IDServer;
 
-use GuzzleHttp\Client as HttpClient;
 use Illuminate\Support\Str;
 
 class Client
 {
-    /**
-     * @var array
-     */
-    protected $config;
-
-    /**
-     * @var Client
-     */
-    protected $httpClient;
-
-    /**
-     * @param array $config
-     */
-    public function __construct(array $config)
-    {
-        $this->config = $config;
-        $this->httpClient = $this->createHttpClient($config);
-    }
-
-    /**
-     * @param array $config
-     * @return HttpClient
-     */
-    private function createHttpClient(array $config): HttpClient
-    {
-        return new HttpClient($config);
-    }
-
     /**
      * @param string $name
      * @return mixed
