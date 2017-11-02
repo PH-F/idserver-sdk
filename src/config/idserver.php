35b763f1588da6d6da001c6d945ae9d4cf1bfdfa
Junior Grossi <juniorgro@gmail.com>
2017-11-02 10:24:10 -0200

    Update config file

--- src/config/idserver.php
+++ src/config/idserver.php
@@ -1,5 +1,19 @@
 <?php
 
 return [
-    'url' => 'http://example.com',
+
+    /*
+    |--------------------------------------------------------------------------
+    | IDServer base URL
+    |--------------------------------------------------------------------------
+    |
+    | This should be set with the IDServer API base endpoint. So
+    | if you would like to get all users using http://example.com/api/v1/users
+    | this option should be set to `http://example.com/api/v1/` and
+    | just use `/users` to get all users.
+    |
+    */
+
+    'url' => 'http://idserver.xingo.nl/v1/',
+
 ];
