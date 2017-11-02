413b38c6f219e91a68d0e498388a400058249cb5
Junior Grossi <juniorgro@gmail.com>
2017-11-02 13:02:39 -0200

    Implement user creating in the SDK

--- src/Resources/User.php
+++ src/Resources/User.php
@@ -2,7 +2,23 @@
 
 namespace Xingo\IDServer\Resources;
 
+use Xingo\IDServer\Entities\Entity;
+use Xingo\IDServer\Entities\User as UserEntity;
+
 class User extends Resource
 {
+    /**
+     * @param array $attributes
+     * @return Entity|UserEntity
+     */
+    public function create(array $attributes): UserEntity
+    {
+        $response = $this->call('POST', '/users', [
+            'form_params' => [$attributes],
+        ]);
 
+        if ($response['status'] === 201) {
+            return $this->makeEntity($response['data']);
+        }
+    }
 }
