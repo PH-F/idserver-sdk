8a78e189d939b07c4284f17e6106a082680456a5
Junior Grossi <juniorgro@gmail.com>
2017-11-03 12:54:07 -0200

    Create validation exception

---
+++ src/Exceptions/ValidationException.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Xingo\IDServer\Exceptions;
+
+use Exception;
+
+class ValidationException extends Exception
+{
+    /**
+     * @var array
+     */
+    public $errors = [];
+
+    /**
+     * @param array $errors
+     */
+    public function __construct(array $errors)
+    {
+        $this->errors = $errors;
+        parent::__construct('Validation failed', 422);
+    }
+}
