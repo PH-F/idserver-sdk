60538ec208504e5544a2339b8e0379e9e7f292a9
Junior Grossi <juniorgro@gmail.com>
2017-11-06 10:11:17 -0200

    Update config file to include client id and secret key

--- config/idserver.php
+++ config/idserver.php
@@ -16,4 +16,43 @@ return [
 
     'url' => 'http://idserver.xingo.nl/v1/',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Store Authentication
+    |--------------------------------------------------------------------------
+    |
+    | Each Store has to be authenticated to ensure the request is coming
+    | from a valid client. The authentication is made with a combination of
+    | a "Client ID" and a "Secret Key".
+    |
+    */
+
+    'store' => [
+
+        /*
+        |--------------------------------------------------------------------------
+        | Client ID
+        |--------------------------------------------------------------------------
+        |
+        | The Client ID is used to identify the Store making the call to the
+        | API. It's a required value.
+        |
+        */
+
+        'client_id' => '',
+
+        /*
+        |--------------------------------------------------------------------------
+        | Secret Key
+        |--------------------------------------------------------------------------
+        |
+        | This is the key associated with the "Client ID". Both field should
+        | match.
+        |
+        */
+
+        'secret_key' => '',
+
+    ]
+
 ];
