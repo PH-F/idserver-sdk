8f13deca9a8de928d7c1b6b4956983a2151427e3
Junior Grossi <juniorgro@gmail.com>
2017-11-06 11:15:47 -0200

    Make sure the all requests have the store credentials

---
+++ tests/Unit/RequestTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit;
+
+use GuzzleHttp\Client;
+use Illuminate\Foundation\Application;
+use Tests\TestCase;
+
+class RequestTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_adds_the_store_headers_to_the_request()
+    {
+        /** @var Client $httpClient */
+        $httpClient = app()->make(Client::class);
+        $headers = $httpClient->getConfig('headers');
+
+        $this->assertArrayHasKey('X-XINGO-Client-ID', $headers);
+        $this->assertArrayHasKey('X-XINGO-Secret-Key', $headers);
+    }
+
+    /**
+     * @param Application $app
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        parent::getEnvironmentSetUp($app);
+        $app['config']->set('idserver.store.client_id', 'foo');
+        $app['config']->set('idserver.store.secret_key', 'bar');
+    }
+}
