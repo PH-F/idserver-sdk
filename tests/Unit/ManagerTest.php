17bcc66a6dc2b1ed26b815bb5a0b14dcafb163d0
Junior Grossi <juniorgro@gmail.com>
2017-11-09 10:54:02 -0200

    Move a test case to the same ManagerTest class

--- tests/Unit/ManagerTest.php
+++ tests/Unit/ManagerTest.php
@@ -2,14 +2,14 @@
 
 namespace Tests\Unit;
 
+use GuzzleHttp\Client;
+use Illuminate\Foundation\Application;
 use Tests\TestCase;
 use Xingo\IDServer\Resources\User;
 
 class ManagerTest extends TestCase
 {
-    /**
-     * @test
-     */
+    /** @test */
     public function it_gets_the_correct_resource_using_magic_methods()
     {
         $manager = app('idserver.manager');
@@ -33,5 +33,30 @@ class ManagerTest extends TestCase
         $manager = app('idserver.manager');
 
         $this->assertEquals('', $manager->getToken());
+    }
+
+    /** @test */
+    public function it_adds_the_store_headers_to_the_request()
+    {
+        /** @var Client $httpClient */
+        $httpClient = app()->make('idserver.client');
+
+        $headers = $httpClient->getConfig('headers');
+
+        $this->assertArrayHasKey('X-XINGO-Client-ID', $headers);
+        $this->assertArrayHasKey('X-XINGO-Secret-Key', $headers);
+        $this->assertEquals('foo', $headers['X-XINGO-Client-ID']);
+        $this->assertEquals('bar', $headers['X-XINGO-Secret-Key']);
+    }
+
+    /**
+     * @param Application $app
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        parent::getEnvironmentSetUp($app);
+
+        $app['config']->set('idserver.store.client_id', 'foo');
+        $app['config']->set('idserver.store.secret_key', 'bar');
     }
 }
