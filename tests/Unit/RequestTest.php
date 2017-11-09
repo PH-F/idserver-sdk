5d5539ba2505568262f9e3705ef107fcbe053001
Junior Grossi <juniorgro@gmail.com>
2017-11-09 10:52:03 -0200

    Remove unnecessary tests. Already tests in other test case.

--- tests/Unit/RequestTest.php
+++ tests/Unit/RequestTest.php
@@ -11,64 +11,17 @@ class RequestTest extends TestCase
 {
     use MockResponse;
 
-    /**
-     * @test
-     */
+    /** @test */
     public function it_adds_the_store_headers_to_the_request()
     {
         /** @var Client $httpClient */
-        $httpClient = app()->make(Client::class);
+        $httpClient = app()->make('idserver.client');
         $headers = $httpClient->getConfig('headers');
 
         $this->assertArrayHasKey('X-XINGO-Client-ID', $headers);
         $this->assertArrayHasKey('X-XINGO-Secret-Key', $headers);
         $this->assertEquals('foo', $headers['X-XINGO-Client-ID']);
         $this->assertEquals('bar', $headers['X-XINGO-Secret-Key']);
-    }
-
-    /**
-     * @test
-     */
-    public function it_adds_the_jwt_to_the_request()
-    {
-        session()->put('jwt_token', 'abc123');
-
-        /** @var Client $httpClient */
-        $httpClient = app()->make(Client::class);
-
-        $headers = $httpClient->getConfig('headers');
-
-        $this->assertArrayHasKey('Authorization', $headers);
-        $this->assertEquals('Bearer abc123', $headers['Authorization']);
-    }
-
-    /**
-     * @test
-     */
-    public function it_do_not_add_the_jwt_if_its_not_in_the_session()
-    {
-        /** @var Client $httpClient */
-        $httpClient = app()->make(Client::class);
-
-        $headers = $httpClient->getConfig('headers');
-
-        $this->assertArrayNotHasKey('Authorization', $headers);
-    }
-
-    /**
-     * @test
-     */
-    public function the_next_request_after_login_has_the_jwt()
-    {
-        $this->mockResponse(200, ['token' => 'jwt123', 'data' => []]);
-        $this->client->users->login('foo@bar.com', 'secret');
-
-        /** @var Client $httpClient */
-        $httpClient = app()->make(Client::class);
-        $headers = $httpClient->getConfig('headers');
-
-        $this->assertArrayHasKey('Authorization', $headers);
-        $this->assertEquals('jwt123', $headers['Authorization']);
     }
 
     /**
