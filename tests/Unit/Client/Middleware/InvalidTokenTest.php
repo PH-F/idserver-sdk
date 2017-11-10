a9a542c69a342d198927622acbbfbc364ad32287
Junior Grossi <juniorgro@gmail.com>
2017-11-10 12:20:15 -0200

    Make sure the JWT is refresh in the whole process

---
+++ tests/Unit/Client/Middleware/InvalidTokenTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit\Client\Middleware;
+
+use Tests\Concerns\MockResponse;
+use Tests\TestCase;
+use Xingo\IDServer\Manager;
+
+class InvalidTokenTest extends TestCase
+{
+    use MockResponse;
+
+    /** @test */
+    function it_will_refresh_the_token_if_it_is_invalid()
+    {
+        $this->mockResponse(200, ['token_invalid']);
+        $this->mockResponse(204, [], ['Authentication' => 'Bearer valid-token']);
+
+        /** @var Manager $manager */
+        $manager = app('idserver.manager');
+        $manager->setToken('invalid-token');
+
+        $manager->users->create([]);
+
+        $this->assertEquals('valid-token', $manager->getToken());
+    }
+}
