7027b18cf6f8f0904402a7eb74b3e3e0ef7de535
Junior Grossi <juniorgro@gmail.com>
2017-11-10 13:53:15 -0200

    Make sure the same endpoint is called again (WIP)

--- tests/Unit/Client/Middleware/InvalidTokenTest.php
+++ tests/Unit/Client/Middleware/InvalidTokenTest.php
@@ -15,13 +15,16 @@ class InvalidTokenTest extends TestCase
     {
         $this->mockResponse(200, ['token_invalid']);
         $this->mockResponse(204, [], ['Authentication' => 'Bearer valid-token']);
+        $this->mockResponse(201, ['data' => ['email' => 'john@example.com']]);
 
         /** @var Manager $manager */
         $manager = app('idserver.manager');
         $manager->setToken('invalid-token');
 
-        $manager->users->create([]);
+        // TODO fix this. It's not calling the same endpoint again
+        $user = $manager->users->create([]);
 
         $this->assertEquals('valid-token', $manager->getToken());
+        $this->assertEquals('john@example.com', $user->email);
     }
 }
