736bd960bd8dd01a24afb3483ea7ca05cec08edf
Junior Grossi <juniorgro@gmail.com>
2017-11-10 11:33:54 -0200

    Change middleware to work with the response interface

--- src/Client/Middleware/InvalidToken.php
+++ src/Client/Middleware/InvalidToken.php
@@ -3,8 +3,8 @@
 namespace Xingo\IDServer\Client\Middleware;
 
 use Closure;
-use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 use Xingo\IDServer\Manager;
 
 class InvalidToken
@@ -16,22 +16,23 @@ class InvalidToken
     public function __invoke(callable $handler)
     {
         return function (RequestInterface $request, array $options) use ($handler) {
-            $response = $handler($request, $options);
+            return $handler($request, $options)->then(
+                function (ResponseInterface $response) {
+                    if ($this->tokenInvalid($response)) {
+                        $this->refreshToken();
+                    }
 
-            if ($this->tokenInvalid($response)) {
-                $this->refreshToken();
-                $response = $handler($request, $options);
-            }
-
-            return $response;
+                    return $response;
+                }
+            );
         };
     }
 
     /**
-     * @param Response $response
+     * @param ResponseInterface $response
      * @return bool
      */
-    private function tokenInvalid(Response $response): bool
+    private function tokenInvalid(ResponseInterface $response): bool
     {
         $json = $response->getBody()->asJson();
 
