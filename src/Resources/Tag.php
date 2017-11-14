d900c0895642632919ea4e17d124e731ee6fdf9b
Junior Grossi <juniorgro@gmail.com>
2017-11-14 11:03:24 -0200

    Add tag creation action

--- src/Resources/Tag.php
+++ src/Resources/Tag.php
@@ -4,8 +4,16 @@ namespace Xingo\IDServer\Resources;
 
 class Tag extends NestedResource
 {
-    public function create()
+    /**
+     * @param array|string $tag
+     * @return array
+     */
+    public function create($tag)
     {
+        $this->call('POST', "users/{$this->id}/tags", [
+            'tag' => $tag,
+        ]);
 
+        return collect($this->contents['tags'])->all();
     }
 }
---
+++ tests/Unit/Resources/TagsTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit\Resources;
+
+use Tests\Concerns;
+use Tests\TestCase;
+
+class TagsTest extends TestCase
+{
+    use Concerns\MockResponse;
+
+    /** @test */
+    function it_can_be_created_using_nested_resource()
+    {
+        $this->mockResponse(201, [
+            'tags' => ['foo', 'bar'],
+        ]);
+
+        $tags = $this->manager->users
+            ->tags->create(['foo', 'bar']);
+
+        $this->assertTrue(is_array($tags));
+        $this->assertEquals(['foo', 'bar'], $tags);
+    }
+}
