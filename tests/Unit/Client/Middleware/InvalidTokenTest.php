d14c210a801cb11e67250a7821b1ca24e71db367
Junior Grossi <juniorgro@gmail.com>
2017-11-21 09:31:11 -0200

    Add a new test case for the API token response

--- tests/Unit/Client/Middleware/InvalidTokenTest.php
+++ tests/Unit/Client/Middleware/InvalidTokenTest.php
@@ -13,7 +13,7 @@ class InvalidTokenTest extends TestCase
     /** @test */
     function it_will_refresh_the_token_if_it_is_invalid()
     {
-        $this->mockResponse(200, ['token_invalid']);
+        $this->mockResponse(200, ['token_expired']);
         $this->mockResponse(204, [], ['Authorization' => 'Bearer valid-token']);
         $this->mockResponse(201, ['data' => ['email' => 'john@example.com']]);
 
@@ -25,5 +25,19 @@ class InvalidTokenTest extends TestCase
 
         $this->assertEquals('valid-token', $manager->getToken());
         $this->assertEquals('john@example.com', $user->email);
+    }
+
+    /** @test */
+    function it_will_not_refresh_the_token_with_normal_response()
+    {
+        $this->mockResponse(201);
+
+        /** @var Manager $manager */
+        $manager = app('idserver.manager');
+        $manager->setToken('valid-token');
+
+        $manager->users->create([]);
+
+        $this->assertEquals('valid-token', $manager->getToken());
     }
 }
