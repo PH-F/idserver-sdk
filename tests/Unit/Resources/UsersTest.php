0387de8f046a5b3114520373111424ea3f0d3460
Junior Grossi <juniorgro@gmail.com>
2017-11-29 09:22:42 -0200

    Add a test case for users pagination

--- tests/Unit/Resources/UsersTest.php
+++ tests/Unit/Resources/UsersTest.php
@@ -100,6 +100,35 @@ class UsersTest extends TestCase
     }
 
     /** @test */
+    function it_paginates_all_users()
+    {
+        $this->mockResponse(200, [
+            'data' => [
+                ['id' => 2],
+            ],
+            'meta' => [
+                'current_page' => 2,
+                'per_page' => 1,
+                'total' => 3
+            ]
+        ]);
+
+        $collection = $this->manager->users->all(2, 1);
+
+        $this->assertCount(1, $collection);
+        $this->assertEquals(2, $collection->first()->id);
+        $this->assertInstanceOf('stdClass', $collection->meta);
+        $this->assertEquals(1, $collection->meta->per_page);
+        $this->assertEquals(3, $collection->meta->total);
+
+        $this->assertRequest(function (Request $request) {
+            $this->assertEquals('GET', $request->getMethod());
+            $this->assertEquals('users', $request->getUri()->getPath());
+            $this->assertEquals('page=2&per_page=1', $request->getUri()->getQuery());
+        });
+    }
+
+    /** @test */
     function it_updates_a_user_with_a_200_status()
     {
         $this->mockResponse(200, [
