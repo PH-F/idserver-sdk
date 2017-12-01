30295556330f628a1e2d0e154d4bb3671c5a54ba
Junior Grossi <juniorgro@gmail.com>
2017-12-01 11:00:52 -0200

    Fix test to expect a different exception

--- tests/Unit/Middleware/AuthTest.php
+++ tests/Unit/Middleware/AuthTest.php
@@ -5,7 +5,7 @@ namespace Tests\Unit\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Tests\TestCase;
-use Xingo\IDServer\Exceptions\AuthorizationException;
+use Xingo\IDServer\Exceptions\MissingJwtException;
 use Xingo\IDServer\Manager;
 use Xingo\IDServer\Middleware\Auth;
 
@@ -14,7 +14,7 @@ class AuthTest extends TestCase
     /** @test */
     function it_throws_an_exception_if_there_is_no_jwt_in_the_session()
     {
-        $this->expectException(AuthorizationException::class);
+        $this->expectException(MissingJwtException::class);
 
         $middleware = new Auth();
         $middleware->handle(new Request(), $this->closure());
