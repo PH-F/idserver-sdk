8151e5302519b4cfcf327a01a01b6efcf2c6f513
Arjan Westdorp <arjanwestdorp@gmail.com>
2017-12-18 18:43:31 +0100

    Added user name method

--- src/Entities/User.php
+++ src/Entities/User.php
@@ -16,4 +16,20 @@ class User extends Entity
     {
         return session()->get('jwt_token');
     }
+
+    /**
+     * Get the full name of the user.
+     *
+     * @return string
+     */
+    public function name()
+    {
+        $names = array_map('trim', [
+            $this->first_name,
+            $this->middle_name,
+            $this->last_name,
+        ]);
+
+        return implode(' ', array_filter($names));
+    }
 }
