c60432c396f3c39fd92a448c0b2e4f33cf0acd30
Arjan <arjanwestdorp@gmail.com>
2018-01-10 17:11:51 +0100

    Refactored address resource to blueprint

--- src/Resources/Address.php
+++ src/Resources/Address.php
@@ -3,55 +3,14 @@
 namespace Xingo\IDServer\Resources;
 
 use Xingo\IDServer\Concerns\NestedResource;
+use Xingo\IDServer\Concerns\ResourceBlueprint;
 use Xingo\IDServer\Contracts\IdsEntity;
 use Xingo\IDServer\Entities;
 
 class Address extends Resource
 {
     use NestedResource;
-
-    /**
-     * @return Collection
-     */
-    public function all(): Collection
-    {
-        $query = $this->organizerQuery();
-
-        $this->call('GET', 'addresses', $query);
-
-        return $this->makeCollection();
-    }
-
-    /**
-     * @return IdsEntity
-     */
-    public function get(): IdsEntity
-    {
-        $this->call('GET', "addresses/$this->id");
-
-        return $this->makeEntity();
-    }
-
-    /**
-     * @param array $attributes
-     * @return Entities\Address
-     */
-    public function update(array $attributes): IdsEntity
-    {
-        $this->call('PUT', "addresses/$this->id", $attributes);
-
-        return $this->makeEntity();
-    }
-
-    /**
-     * @return bool
-     */
-    public function delete(): bool
-    {
-        $response = $this->call('DELETE', "addresses/$this->id");
-
-        return 204 === $response->getStatusCode();
-    }
+    use ResourceBlueprint;
 
     /**
      * @param array $attributes
