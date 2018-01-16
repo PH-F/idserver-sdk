4f0665ecdeb670c5ffc3fa91f28a2ea5a23565d0
Arjan <arjanwestdorp@gmail.com>
2018-01-16 10:15:49 +0100

    Implement abilities resource

---
+++ tests/Unit/Resources/AbilitiesTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Unit\Resources;
+
+use GuzzleHttp\Psr7\Request;
+use Tests\Concerns;
+use Tests\TestCase;
+use Xingo\IDServer\Contracts\IdsEntity;
+use Xingo\IDServer\Entities;
+use Xingo\IDServer\Resources;
+
+class AbilitiesTest extends TestCase
+{
+    use Concerns\MockResponse;
+
+    /** @test */
+    public function it_lists_all_abilities()
+    {
+        $this->mockResponse(200, [
+            'data' => [
+                ['id' => 1],
+                ['id' => 2],
+            ],
+        ]);
+
+        $collection = $this->manager->abilities->all();
+
+        $this->assertInstanceOf(Resources\Collection::class, $collection);
+        $this->assertCount(2, $collection);
+        $this->assertInstanceOf(Entities\Ability::class, $collection->first());
+        $this->assertInstanceOf(IdsEntity::class, $collection->first());
+        $this->assertEquals(2, $collection->last()->id);
+
+        $this->assertRequest(function (Request $request) {
+            $this->assertEquals('abilities', $request->getUri()->getPath());
+            $this->assertEquals('page=1&per_page=10', $request->getUri()->getQuery());
+        });
+    }
+}
