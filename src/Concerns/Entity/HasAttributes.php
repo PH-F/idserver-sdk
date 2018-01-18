a79e5c51b782c74004e8788a3ee3e18625aede11
Junior Grossi <juniorgro@gmail.com>
2018-01-18 11:08:50 -0200

    Extract some logic into traits

---
+++ src/Concerns/Entity/HasAttributes.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Xingo\IDServer\Concerns\Entity;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Concerns\HasAttributes as BaseHasAttributes;
+
+trait HasAttributes
+{
+    use BaseHasAttributes {
+        asDateTime as protected baseAsDateTime;
+    }
+
+    /**
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * @param string $name
+     * @return mixed|null
+     */
+    public function __get(string $name)
+    {
+        return $this->getAttribute($name);
+    }
+
+    /**
+     * @param string $name
+     * @param mixed $value
+     */
+    public function __set(string $name, $value)
+    {
+        $this->setAttribute($name, $value);
+    }
+
+    /**
+     * @return bool
+     */
+    public function getIncrementing()
+    {
+        return $this->incrementing;
+    }
+
+    /**
+     * @param mixed $value
+     * @return Carbon
+     */
+    protected function asDateTime($value)
+    {
+        if (is_array($value)) {
+            return Carbon::parse(array_get($value, 'date'))
+                ->timezone(array_get($value, 'timezone'));
+        }
+
+        return $this->baseAsDateTime($value);
+    }
+}
