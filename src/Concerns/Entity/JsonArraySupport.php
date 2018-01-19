c19a0878e9fce80fccc1ca0861b571b2b526d92c
Arjan <arjanwestdorp@gmail.com>
2018-01-19 09:04:23 +0100

    Fix isset/unset support

--- src/Concerns/Entity/JsonArraySupport.php
+++ src/Concerns/Entity/JsonArraySupport.php
@@ -56,37 +56,69 @@ trait JsonArraySupport
     }
 
     /**
-     * @param mixed $offset
+     * Determine if the given attribute exists.
+     *
+     * @param  mixed  $offset
      * @return bool
      */
     public function offsetExists($offset)
     {
-        return isset($this->$offset);
+        return ! is_null($this->getAttribute($offset));
     }
 
     /**
-     * @param mixed $offset
-     * @return mixed|null
+     * Get the value for a given offset.
+     *
+     * @param  mixed  $offset
+     * @return mixed
      */
     public function offsetGet($offset)
     {
-        return $this->$offset;
+        return $this->getAttribute($offset);
     }
 
     /**
-     * @param mixed $offset
-     * @param mixed $value
+     * Set the value for a given offset.
+     *
+     * @param  mixed  $offset
+     * @param  mixed  $value
+     * @return void
      */
     public function offsetSet($offset, $value)
     {
-        $this->$offset = $value;
+        $this->setAttribute($offset, $value);
     }
 
     /**
-     * @param mixed $offset
+     * Unset the value for a given offset.
+     *
+     * @param  mixed  $offset
+     * @return void
      */
     public function offsetUnset($offset)
     {
-        unset($this->$offset);
+        unset($this->attributes[$offset], $this->relations[$offset]);
+    }
+
+    /**
+     * Determine if an attribute or relation exists on the model.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    public function __isset($key)
+    {
+        return $this->offsetExists($key);
+    }
+
+    /**
+     * Unset an attribute on the model.
+     *
+     * @param  string  $key
+     * @return void
+     */
+    public function __unset($key)
+    {
+        $this->offsetUnset($key);
     }
 }
