d3dfee51fc930c7d19875eb9e6a7567afbf6d7a3
Arjan <arjanwestdorp@gmail.com>
2018-12-11 19:52:10 +0100

    Print dd output from API

--- src/Client/Support/JsonStream.php
+++ src/Client/Support/JsonStream.php
@@ -27,6 +27,8 @@ class JsonStream implements StreamInterface, JsonSerializable
         $decodedContent = json_decode($content, true);
 
         if (json_last_error() !== JSON_ERROR_NONE) {
+            $this->printDumpAndDieOutput($content);
+
             throw new RuntimeException(
                 'Error trying to decode response: ' .
                 json_last_error_msg()
@@ -44,5 +46,19 @@ class JsonStream implements StreamInterface, JsonSerializable
     public function asJson()
     {
         return $this->jsonSerialize();
+    }
+
+    /**
+     * Print the output to the screen when app is running locally and
+     * the output seems to be a dd response.
+     *
+     * @param string $content
+     */
+    protected function printDumpAndDieOutput($content)
+    {
+        if (app()->isLocal() && preg_match('/"sf-dump-[0-9]+"/', $content)) {
+            echo $content;
+            exit;
+        }
     }
 }
