54c052de2b20fdbb53294e8e5ce3343732f8606f
Arjan <arjanwestdorp@gmail.com>
2019-06-07 22:44:14 +0200

    Fix attempt

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,17 +10,18 @@ class TestCase extends BaseTestCase
     /**
      * Setup the test case and register test directory to be able to load stubs.
      */
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
 
         if (!defined('TEST_PATH')) {
-            define('TEST_PATH', __DIR__ . '/');
+            define('TEST_PATH', __DIR__.'/');
         }
     }
 
     /**
-     * @param \Illuminate\Foundation\Application $app
+     * @param  \Illuminate\Foundation\Application  $app
+     *
      * @return array
      */
     protected function getPackageProviders($app): array
