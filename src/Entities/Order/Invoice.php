a67793a1f5a248ec5015f0dc516aaff5fb807cac
patrick <github@ph-f.nl>
2020-09-10 11:07:01 +0200

    Add priceable trait to invoice entity

--- src/Entities/Order/Invoice.php
+++ src/Entities/Order/Invoice.php
@@ -3,7 +3,9 @@
 namespace Xingo\IDServer\Entities\Order;
 
 use Xingo\IDServer\Entities\Entity;
+use Xingo\IDServer\Entities\Traits\Priceable;
 
 class Invoice extends Entity
 {
+    use Priceable;
 }
