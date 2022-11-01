9015c01881223e253b12f3f140eb76bc3556ea69
Patrick <patrick.houtman@xingo.nl>
2022-11-01 14:37:48 +0100

    Add import to coupon

--- src/Resources/Coupon.php
+++ src/Resources/Coupon.php
@@ -3,6 +3,8 @@
 namespace Xingo\IDServer\Resources;
 
 use Xingo\IDServer\Concerns\ResourceBlueprint;
+use Xingo\IDServer\Contracts\IdsEntity;
+use Xingo\IDServer\Entities;
 
 /**
  * Class Coupon
@@ -12,4 +14,19 @@ use Xingo\IDServer\Concerns\ResourceBlueprint;
 class Coupon extends Resource
 {
     use ResourceBlueprint;
+
+    /**
+     * Import bank transactions into the idserver.
+     *
+     * @param $data
+     *
+     * @return IdsEntity
+     */
+    public function import($data): IdsEntity
+    {
+        $this->asMultipart()->call('POST', 'coupons/import', $data);
+
+        return $this->makeEntity(null, Entities\Coupon::class);
+    }
+
 }
